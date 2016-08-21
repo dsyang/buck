a81c6c9b882b2f5f7b95c9bfe5654f1c5d5176f0
Dan Yang <dsyang92@gmail.com>
2016-08-21 16:09:07 -0700

    convert config to kotlin

---
+++ src/com/facebook/buck/jvm/kotlin/config/KotlinBuckConfig.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.facebook.buck.jvm.kotlin.config
+
+import com.facebook.buck.cli.BuckConfig
+import com.facebook.buck.io.ExecutableFinder
+import com.facebook.buck.rules.HashedFileTool
+import com.facebook.buck.rules.Tool
+import com.google.common.base.Supplier
+import com.google.common.base.Suppliers
+
+import java.nio.file.Paths
+
+class KotlinBuckConfig(private val delegate: BuckConfig) {
+
+    val kotlinCompiler: Supplier<Tool>
+        get() {
+            val compilerPath = delegate.getPath("kotlin", "compiler").or(DEFAULT_KOTLIN_COMPILER)
+
+            val compiler = ExecutableFinder().getExecutable(compilerPath, delegate.environment)
+
+            return Suppliers.ofInstance<Tool>(HashedFileTool(compiler))
+        }
+
+    companion object {
+        private val DEFAULT_KOTLIN_COMPILER = Paths.get("kotlinc")
+    }
+}
